27173ace41fbb73730165229dc0a6543f843d9e3
WK WK <witold.kepczynski@gmail.com>
2021-10-06 23:22:42 +0200

    cleanup

--- LockerClient.Example/Program.cs
+++ LockerClient.Example/Program.cs
@@ -45,13 +45,11 @@ namespace LockerExample
             await _locker.AuthorizeAsync("witek", "test123");
             var lockerId = await _locker.AddLockerAsync();
 
-            //var lockerId = Guid.Parse("7ff9bf42-cb8c-4100-a900-4a8bad90638d");
-
             _logger.Information("Attached to {lockerId}", lockerId);
 
             _locker.UseLocker(lockerId);
 
-            _locker["yello"] = Encoding.UTF8.GetBytes("witek jest mistrz nad mistrze nikt tego nie moze podwazyc");
+            _locker["yello"] = Encoding.UTF8.GetBytes("dummy data");
 
             _logger.Information("Got {data}, for {key}", Encoding.UTF8.GetString(_locker["yello"]), "yello");
         }
